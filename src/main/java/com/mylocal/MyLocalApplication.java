c8a27975b8b6caf66f4f9392ea832c81bd7f2c0b
dell <null>
2021-08-11 15:42:04 +0800

    第一次提交

---
+++ src/main/java/com/mylocal/MyLocalApplication.java
@@ -0,0 +1,13 @@
+package com.mylocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLocalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLocalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mylocal/MylocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylocalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
